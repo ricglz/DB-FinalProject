46dd09f4ee3dcf3c2888795b264c55f2371b348c
ricglz0201 <rjgcastillo@hotmail.com>
2018-11-02 19:40:42 -0600

    Fix insert into

--- create_tables.sql
+++ create_tables.sql
@@ -112,140 +112,93 @@ CREATE TABLE Response (
 ----------------------------------------------------------------------
 
 
-INSERT INTO Patient (fName, lName, sex, telephone, street, zip, state, city, birthDate)
-VALUES ('John', 'Smith', 'M', '8112345678', 'Main Street', '64800', 'NL', 'Monterrey', '1986-04-05')
+INSERT INTO Patient VALUES (1, 'John', 'Smith', 'M', '8112345678', 'Main Street', '64800', 'NL', 'Monterrey', '1986-04-05')
 
-INSERT INTO Patient (fName, lName, sex, telephone, street, zip, state, city, birthDate)
-VALUES ('Margaret', 'Smith', 'F', '8187654321', 'Main Street', '64800', 'NL', 'Monterrey', '1986-08-10')
+INSERT INTO Patient VALUES (2, 'Margaret', 'Smith', 'F', '8187654321', 'Main Street', '64800', 'NL', 'Monterrey', '1986-08-10')
 
-INSERT INTO Patient (fName, lName, sex, telephone, street, zip, state, city, birthDate) 
-VALUES ('Hugh', 'Jackman', 'M', '5412345678', '2nd Street', '72100', 'NL', 'San Pedro', '1987-12-04')
+INSERT INTO Patient VALUES (3, 'Hugh', 'Jackman', 'M', '5412345678', '2nd Street', '72100', 'NL', 'San Pedro', '1987-12-04')
 
-INSERT INTO Patient (fName, lName, sex, telephone, street, zip, state, city, birthDate) 
-VALUES ('Jason', 'Boise', 'M', '8100000000', 'Third Street', '60100', 'NL', 'San Nicolas', '1970-10-25')
+INSERT INTO Patient VALUES (4, 'Jason', 'Boise', 'M', '8100000000', 'Third Street', '60100', 'NL', 'San Nicolas', '1970-10-25')
 
 ----------------------------------------------------------------------
 
-INSERT INTO HD_Question ( description ) 
-VALUES ('Humor depresivo (tristeza, desesperanza, desamparo, sentimiento de inutilidad)')
+INSERT INTO Test VALUES (1, 'Depression')
+INSERT INTO Test VALUES (2, 'Anxiety')
 
-INSERT INTO HD_Question ( description ) 
-VALUES ('Sentimientos de culpa')
+INSERT INTO Question VALUES (101, 'Depressed mood', 1)
+INSERT INTO Question VALUES (102, 'Depressed mood', 1)
+INSERT INTO Question VALUES (103, 'Depressed mood', 1)
+INSERT INTO Question VALUES (104, 'Depressed mood', 1)
+INSERT INTO Question VALUES (105, 'Depressed mood', 1)
+INSERT INTO Question VALUES (201, 'Depressed mood', 2)
+INSERT INTO Question VALUES (202, 'Depressed mood', 2)
+INSERT INTO Question VALUES (203, 'Depressed mood', 2)
+INSERT INTO Question VALUES (204, 'Depressed mood', 2)
+INSERT INTO Question VALUES (205, 'Depressed mood', 2)
 
-INSERT INTO HD_Question ( description ) 
-VALUES ('Suicidio')
+INSERT INTO Instance VALUES (1024, 1)
+INSERT INTO Instance VALUES (1025, 2)
+INSERT INTO Instance VALUES (1026, 1)
+INSERT INTO Instance VALUES (1027, 2)
+INSERT INTO Instance VALUES (1028, 1)
+INSERT INTO Instance VALUES (1029, 2)
+INSERT INTO Instance VALUES (1030, 1)
+INSERT INTO Instance VALUES (1031, 2)
 
-INSERT INTO HD_Question ( description ) 
-VALUES ('Insomnio precoz')
-
-INSERT INTO HD_Question ( description ) 
-VALUES ('Insomnio intermedio')
-
-INSERT INTO HD_Question ( description ) 
-VALUES ('Insomnio tardio')
-
-INSERT INTO HD_Question ( description ) 
-VALUES ('Trabajo y actividades')
-
-INSERT INTO HD_Question ( description ) 
-VALUES ('Inhibicion psicomotora (lentitud de pensamiento y lenguaje, facultad de
-concentracion disminuida, disminucion de la actividad motora)')
-
-INSERT INTO HD_Question ( description ) 
-VALUES ('Ansiedad psiquica')
-
-INSERT INTO HD_Question ( description ) 
-VALUES ('Ansiedad somatica (signos fisicos de ansiedad: gastrointestinales: sequedad de
-boca, diarrea, eructos, indigestion, etc; cardiovasculares: palpitaciones, cefaleas;
-respiratorios: hiperventilacion, suspiros; frecuencia de miccion incrementada;
-transpiracion)')
-
-----------------------------------------------------------------------
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('P', 'Estado de animo ansioso.
-Preocupaciones, anticipacion de lo peor, aprensi�n
-(anticipacion temerosa), irritabilidad')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('P', 'Tension.
-Sensacipn de tension, imposibilidad de relajarse,
-reacciones con sobresalto, llanto facil, temblores,
-sensacion de inquietud.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('P', 'Temores.
-A la oscuridad, a los desconocidos, a quedarse solo, a los
-animales grandes, al trafico, a las multitudes.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('P', 'Insomnio.
-Dificultad para dormirse, sueño interrumpido, sueño
-insatisfactorio y cansancio al despertar.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('P', 'Intelectual (cognitivo)
-Dificultad para concentrarse, mala memoria.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('P', 'Estado de animo deprimido.
-Perdida de interes, insatisfaccion en las diversiones,
-depresion, despertar prematuro, cambios de humor
-durante el dia.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas somaticos generales (musculares)
-Dolores y molestias musculares, rigidez muscular,
-contracciones musculares, sacudidas clonicas, crujir de
-dientes, voz temblorosa.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas somaticos generales (sensoriales)
-Zumbidos de oidos, vision borrosa, sofocos y escalofr�os,
-sensacion de debilidad, sensacion de hormigueo.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas cardiovasculares.
-Taquicardia, palpitaciones, dolor en el pecho, latidos
-vasculares, sensacion de desmayo, extrasistole.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas respiratorios.
-Opresion o constriccion en el pecho, sensacion de ahogo,
-suspiros, disnea.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas gastrointestinales.
-Dificultad para tragar, gases, dispepsia: dolor antes y
-despues de comer, sensacion de ardor, sensacion de
-estomago lleno, vomitos acuosos, vomitos, sensacion de
-estomago vacio, digestion lenta, borborigmos (ruido
-intestinal), diarrea, perdida de peso, estreñimiento.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas genitourinarios.
-Miccion frecuente, miccion urgente, amenorrea,
-menorragia, aparicion de la frigidez, eyaculacion precoz,
-ausencia de ereccion, impotencia.')
-
-INSERT INTO HA_Question ( anxType, description )
-VALUES ('S', 'Sintomas autonomos.
-Boca seca, rubor, palidez, tendencia a sudar, vertigos,
-cefaleas de tension, piloereccion (pelos de punta)')
+INSERT INTO Resonse VALUES (6010, 3, 101, 1024)
+INSERT INTO Resonse VALUES (6011, 3, 102, 1024)
+INSERT INTO Resonse VALUES (6012, 3, 103, 1024)
+INSERT INTO Resonse VALUES (6013, 3, 104, 1024)
+INSERT INTO Resonse VALUES (6014, 3, 105, 1024)
+INSERT INTO Resonse VALUES (6015, 3, 201, 1025)
+INSERT INTO Resonse VALUES (6016, 3, 202, 1025)
+INSERT INTO Resonse VALUES (6017, 3, 203, 1025)
+INSERT INTO Resonse VALUES (6018, 3, 204, 1025)
+INSERT INTO Resonse VALUES (6019, 3, 205, 1025)
+INSERT INTO Resonse VALUES (6020, 3, 101, 1026)
+INSERT INTO Resonse VALUES (6021, 3, 102, 1026)
+INSERT INTO Resonse VALUES (6022, 3, 103, 1026)
+INSERT INTO Resonse VALUES (6023, 3, 104, 1026)
+INSERT INTO Resonse VALUES (6024, 3, 105, 1026)
+INSERT INTO Resonse VALUES (6025, 3, 201, 1027)
+INSERT INTO Resonse VALUES (6026, 3, 202, 1027)
+INSERT INTO Resonse VALUES (6027, 3, 203, 1027)
+INSERT INTO Resonse VALUES (6028, 3, 204, 1027)
+INSERT INTO Resonse VALUES (6029, 3, 205, 1027)
+INSERT INTO Resonse VALUES (6030, 3, 101, 1028)
+INSERT INTO Resonse VALUES (6031, 3, 102, 1028)
+INSERT INTO Resonse VALUES (6032, 3, 103, 1028)
+INSERT INTO Resonse VALUES (6033, 3, 104, 1028)
+INSERT INTO Resonse VALUES (6034, 3, 105, 1028)
+INSERT INTO Resonse VALUES (6035, 3, 201, 1029)
+INSERT INTO Resonse VALUES (6036, 3, 202, 1029)
+INSERT INTO Resonse VALUES (6037, 3, 203, 1029)
+INSERT INTO Resonse VALUES (6038, 3, 204, 1029)
+INSERT INTO Resonse VALUES (6039, 3, 205, 1029)
+INSERT INTO Resonse VALUES (6040, 3, 101, 1030)
+INSERT INTO Resonse VALUES (6041, 3, 102, 1030)
+INSERT INTO Resonse VALUES (6042, 3, 103, 1030)
+INSERT INTO Resonse VALUES (6043, 3, 104, 1030)
+INSERT INTO Resonse VALUES (6044, 3, 105, 1030)
+INSERT INTO Resonse VALUES (6045, 3, 201, 1031)
+INSERT INTO Resonse VALUES (6046, 3, 202, 1031)
+INSERT INTO Resonse VALUES (6047, 3, 203, 1031)
+INSERT INTO Resonse VALUES (6048, 3, 204, 1031)
+INSERT INTO Resonse VALUES (6049, 3, 205, 1031)
 
 ----------------------------------------------------------------------
 
-INSERT INTO Medicament (medName) VALUES ('Celexa')
-INSERT INTO Medicament (medName) VALUES ('Lexapro')
-INSERT INTO Medicament (medName) VALUES ('Paxil')
-INSERT INTO Medicament (medName) VALUES ('Pexeva')
-INSERT INTO Medicament (medName) VALUES ('Prozac')
-INSERT INTO Medicament (medName) VALUES ('Adapin')
+INSERT INTO Medicament VALUES (1, 'Celexa')
+INSERT INTO Medicament VALUES (2, 'Lexapro')
+INSERT INTO Medicament VALUES (3, 'Paxil')
+INSERT INTO Medicament VALUES (4, 'Pexeva')
+INSERT INTO Medicament VALUES (5, 'Prozac')
+INSERT INTO Medicament VALUES (6, 'Adapin')
 
 ----------------------------------------------------------------------
 
-INSERT INTO Diagnosis ( isPrimary, sicknessDegree )VALUES (1,5)
-INSERT INTO Diagnosis ( isPrimary, sicknessDegree )VALUES (0,3)
-INSERT INTO Diagnosis ( isPrimary, sicknessDegree )VALUES (1,6)
-INSERT INTO Diagnosis ( isPrimary, sicknessDegree )VALUES (1,1)
-INSERT INTO Diagnosis ( isPrimary, sicknessDegree )VALUES (1,2)
+INSERT INTO Diagnosis VALUES (1, 1,5)
+INSERT INTO Diagnosis VALUES (2, 0,3)
+INSERT INTO Diagnosis VALUES (3, 1,6)
+INSERT INTO Diagnosis VALUES (4, 1,1)
+INSERT INTO Diagnosis VALUES (5, 1,2)
