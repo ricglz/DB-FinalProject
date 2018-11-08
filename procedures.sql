7d1f956b0fafb101b841edd0a629814533f06549
ricglz0201 <rjgcastillo@hotmail.com>
2018-11-07 22:05:19 -0600

    Extra queries

--- procedures.sql
+++ procedures.sql
@@ -30,10 +30,26 @@ FROM (
 ) t
 LEFT JOIN Test_scale ts ON ts.testId = t.testId AND puntuacion BETWEEN ts.lowLimit AND ts.highLimit
 
---6: INPUT: Id
+--6. INPUT: Id
 SELECT questionTitle, questionInstructions, value FROM Question q
 JOIN Response r ON q.questionId = r.questionId
 WHERE instanceId = Id
+
+--7. Input: patientScore
+SELECT t.*, descriptor
+FROM (
+	SELECT i.instanceId, i.testId, vDate, SUM(value) AS puntuacion
+	FROM Response r JOIN Instance i ON r.instanceId = i.instanceId
+	JOIN Visit v ON i.visitId = v.visitId
+	WHERE puntuacion >= patientScore
+	GROUP BY instanceId
+) t
+LEFT JOIN Test_scale ts ON ts.testId = t.testId AND puntuacion BETWEEN ts.lowLimit AND ts.highLimit
+
+--8. Input: patientName
+SELECT * FROM Patient
+WHERE fName = patientName OR lName = patientName 
+    OR fname + lName = patientName
 
 --Delete Patient EHR
 DELETE FROM Prescription_details WHERE prescriptionId IN (
